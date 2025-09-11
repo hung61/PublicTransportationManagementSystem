056e9167e89b3fa5a9bed29b536ae99388a007f4
hung61 <ngkhhung5326a7@gmail.com>
2025-09-11 16:50:22 +0700

    Add files via upload

---
+++ PublicTransportationManagementSystem.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+using namespace std;
+class Station{
+    private:
+        string name;
+        string location;
+        string type;
+
+    public:
+
+};
+
+class Vehicles{
+    protected:
+        string route;
+        int capacity;
+        string status;
+
+    public:
+
+};
+
+class ExpressBus: public Vehicles{
+    public:
+
+};
+
+class Passengers{
+    private:
+        string name;
+        string ID;
+        string bookedTickets;
+
+    public:
+    
+};
+int main(){
+
+}
